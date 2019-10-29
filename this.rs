654c64ce4182610a17e082126ae85104cc0257cc
bsdpunk <bsdpunk@gmail.com>
2019-10-28 20:08:01 -0500

    FixedIt

--- this.rs
+++ this.rs
@@ -1,9 +1,10 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
     let mut done = false; 
     while !done {
+
+    let mut input = String::new();
         io::stdin().read_line(&mut input)
             .ok()
             .expect("Couldn't read line");    
