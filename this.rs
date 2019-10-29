3fd3497fad58be99a39631cf15b8f44801ea6366
bsdpunk <bsdpunk@gmail.com>
2019-10-28 20:09:15 -0500

    WhiteSpace

--- this.rs
+++ this.rs
@@ -4,12 +4,12 @@ fn main() {
     let mut done = false; 
     while !done {
 
-    let mut input = String::new();
+        let mut input = String::new();
         io::stdin().read_line(&mut input)
             .ok()
             .expect("Couldn't read line");    
         println!("{}", input);
-        
+
         if input.trim() == "quit" {
             done = true;
         }
